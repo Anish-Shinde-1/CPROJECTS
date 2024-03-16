2278417a2b754f2bca7a089afb4b794c9f14074a
Anish-Shinde-1 <shindeanish2004@gmail.com>
2024-03-16 18:50:06 +0530

    Added Exponent function in calculator

--- calculator.c
+++ calculator.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+#define PI 3.14159265358979323846
 
 int options(int opt);
-double arithmetic(double result, double a, double sum, double sub, double prod, double div);
+double arithmetic(double result, double a);
+double exponent(double x, double result, double expo, double base, int opt2);
 
 int main()
 {
-	int opt;
-	double a, result, input, sum, sub, prod, div;
+	int opt, opt2;
+	double a, x, result, base, expo;
 
 	opt=options(opt);
 
@@ -15,9 +20,12 @@ int main()
 	switch(opt)
 	{
 	case 1 : 
-		result = arithmetic(a, result, sum, sub, prod, div);
+		result = arithmetic(a, result);
 		printf("END RESULT : %lf\n", result);
 		break;
+
+	case 2 : 
+		result = exponent(x, result, base, expo, opt2); 
 	}
 
 return 0;
@@ -25,13 +33,16 @@ return 0;
 
 int options(int opt)
 {
+	system("clear");
 	printf("|=========== CALCULATOR ==========|\n");
 	printf("|                                 |\n");
 	printf("| 1. Arithmetic                   |\n");
-	printf("| 2. Exponential                  |\n");
+	printf("| 2. Exponent                     |\n");
 	printf("| 3. Factorial                    |\n");
 	printf("| 4. Trigonometric                |\n");
 	printf("| 5. Unit Converter               |\n");
+	printf("| 6. Matrix                       |\n");
+	printf("| 7. Equations                    |\n");
 	printf("|                                 |\n");
 
 	printf("Which operation do you want to execute?\nAns : ");
@@ -40,7 +51,7 @@ int options(int opt)
 	return opt;
 }
 
-double arithmetic(double result, double a, double sum, double sub, double prod, double div)
+double arithmetic(double result, double a)
 {
 	char operator = 'n';
 
@@ -110,5 +121,62 @@ double arithmetic(double result, double a, double sum, double sub, double prod, 
 
 	printf("\n");
 	
+	return result;
+}
+
+double exponent(double x, double result, double expo, double base, int opt2)
+{
+
+	system("clear");
+	printf("|=========== CALCULATOR ==========|\n");
+	printf("|                                 |\n");
+	printf("| 1. Power                        |\n");
+	printf("| 2. Exponential                  |\n");
+	printf("| 3. Logarithmic                  |\n");
+	printf("|                                 |\n");
+
+	do
+	{
+		printf("\nEnter your choice : ");
+		scanf("%d", &opt2);
+
+		if(opt2<1 || opt2>3)
+		{
+			printf("INVALID OPTION\n");
+
+		}
+
+	} while(opt2<1 || opt2>3);
+
+	if(opt2==1)
+	{
+		printf("Enter the base : ");
+		scanf("%lf", &base);
+
+		printf("Enter the exponent : ");
+		scanf("%lf", & expo);
+
+		result = pow(base,expo);
+
+		printf("END RESULT : %lf\n", result);
+	}
+	else if(opt2==2)
+	{
+		printf("Enter the value : ");
+		scanf("%lf", &x);
+
+		result = exp(x);
+
+		printf("END RESULT : %lf\n", result);
+	}
+	else if(opt2==3)
+	{
+		printf("Enter the value : ");
+		scanf("%lf", &x);
+
+		result = log(x);
+
+		printf("END RESULT : %lf\n", result);
+	}
 	return result;
 }
