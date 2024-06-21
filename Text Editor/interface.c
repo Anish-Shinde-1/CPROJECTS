be0858e415514acc8625d7fa09f7ac29246b3055
Anish-Shinde-1 <shindeanish2004@gmail.com>
2024-06-21 19:50:26 +0530

    now the program can navigate through the file contents displayed on screen ie. scrolling

--- Text Editor/interface.c
+++ Text Editor/interface.c
@@ -18,6 +18,7 @@ int main()
 	initscr();
 	cbreak();
 	refresh();
+	curs_set(0);
 
 	getmaxyx(stdscr, y_max, x_max);
 
@@ -33,9 +34,8 @@ int main()
 	int mode = mode_choice(choice_win);
 
 	char *modes[3] = {"r", "r+", "w+"};
-	mvwprintw(choice_win, 10, 3, "%s", modes[mode]);
+	// mvwprintw(choice_win, 10, 3, "%s", modes[mode]);
 	wrefresh(choice_win);
-	getchar();
 	char file_name[30];
 	char ch;
 
@@ -68,8 +68,9 @@ int main()
 	int lines = count_file_lines(file_ptr);
 
 	WINDOW *text_pad = newpad(500, x_max - 4);
+	keypad(text_pad, true);
 	// box(text_pad, 0, 0);
-	prefresh(text_pad, 0, 0, 2, 2, y_max - 3, x_max - 2);
+	prefresh(text_pad, 0, 0, 2, 2, y_max - 3, x_max - 4);
 
 	switch (mode)
 	{
@@ -101,7 +102,9 @@ void file_input(WINDOW *choice_win, char file_name[30])
 	mvwprintw(choice_win, 0, 3, "[ TEXT EDITOR ]");
 	mvwprintw(choice_win, 2, 3, "[+] FILE NAME : ");
 	wrefresh(choice_win);
+	curs_set(1);
 	wgetnstr(choice_win, file_name, 29);
+	curs_set(0);
 }
 
 int mode_choice(WINDOW *choice_win)
@@ -174,24 +177,138 @@ int count_file_lines(FILE *file_ptr)
 void read_file(FILE *file_ptr, WINDOW *text_pad, int x_max, int y_max, char file_name[30])
 {
 	// NOTE : MAJOR ISSUE WAS THAT I HAD TO OPEN THE FILE IN THIS FUNCTION ONCE AGAIN !!!!!
-	file_ptr = fopen(file_name, "r");
+	// file_ptr = fopen(file_name, "r");
 
-	char buffer[512];
-	prefresh(text_pad, 0, 0, 2, 2, y_max - 3, x_max - 2);
+	char buffer[1024];
+	char temp_buffer[x_max - 9]; // 200 = 199 + null
+	prefresh(text_pad, 0, 0, 2, 2, y_max - 3, x_max - 4);
 
-	int line_num = 0;
-	while (fgets(buffer, sizeof(buffer) - 1, file_ptr) != NULL)
+	int ch;
+	int highlight_line = 0;
+	int start_line = 0;
+	int end_line = y_max - 5;
+
+	do
 	{
-		int len = strlen(buffer);
-		if (len > 0 && buffer[len - 1] == '\n')
+
+		int index = 1;
+		int line = 0; // Used when printing content to the screen
+
+		file_ptr = fopen(file_name, "r");
+
+		while (fgets(buffer, sizeof(buffer) - 1, file_ptr) != NULL)
 		{
-			buffer[len - 1] = '\0';
+			int len = strlen(buffer);
+
+			if (len > (x_max - 10))
+			{
+				int printed = 0;
+				while (len >= (x_max - 10))
+				{
+					strncpy(temp_buffer, buffer + printed, (x_max - 10));
+					temp_buffer[(x_max - 9)] = '\0';
+
+					if (printed == 0)
+					{
+						if (line == highlight_line)
+						{
+							wattron(text_pad, A_STANDOUT);
+						}
+						mvwprintw(text_pad, line, 0, "%3d. %s", index, temp_buffer);
+						wattroff(text_pad, A_STANDOUT);
+					}
+					else
+					{
+						if (line == highlight_line)
+						{
+							wattron(text_pad, A_STANDOUT);
+						}
+						mvwprintw(text_pad, line, 0, "     %s", temp_buffer);
+						wattroff(text_pad, A_STANDOUT);
+					}
+
+					printed += (x_max - 10);
+					len -= (x_max - 10);
+					line++;
+				}
+				if (len > 0)
+				{
+					strcpy(temp_buffer, buffer + printed);
+					temp_buffer[strlen(temp_buffer) - 1] = '\0';
+
+					if (line == highlight_line)
+					{
+						wattron(text_pad, A_STANDOUT);
+					}
+					mvwprintw(text_pad, line, 0, "     %s", temp_buffer);
+					wattroff(text_pad, A_STANDOUT);
+					line++;
+				}
+				index++;
+			}
+			else
+			{
+				if (len > 0 && buffer[len - 1] == '\n')
+				{
+					buffer[len - 1] = '\0';
+				}
+
+				if (line == highlight_line)
+				{
+					wattron(text_pad, A_STANDOUT);
+				}
+
+				mvwprintw(text_pad, line, 0, "%3d. %s", index, buffer);
+				wattroff(text_pad, A_STANDOUT);
+				index++;
+				line++;
+			}
+		}
+		fclose(file_ptr);
+		prefresh(text_pad, start_line, 0, 2, 2, y_max - 3, x_max - 4);
+
+		mvprintw(y_max - 1, (x_max - 60), "[ line : %3d / Total lines : %3d ]", highlight_line + 1, line);
+		wrefresh(stdscr);
+
+		noecho();
+		ch = wgetch(text_pad);
+		echo();
+
+		switch (ch)
+		{
+		case KEY_UP:
+			highlight_line--;
+
+			if (highlight_line < 0)
+				highlight_line = 0;
+
+			if (highlight_line < start_line)
+			{
+				start_line--;
+			}
+
+			break;
+
+		case KEY_DOWN:
+			highlight_line++;
+
+			if (highlight_line >= line)
+				highlight_line = line - 1;
+
+			if (highlight_line > (y_max - 5) && highlight_line < line)
+			{
+				start_line++;
+			}
+
+			if (start_line > line - (y_max - 5))
+			{
+				start_line = line - (y_max - 5);
+			}
+			break;
+
+		default:
+			break;
 		}
 
-		mvwprintw(text_pad, line_num, 0, "%3d. %s", line_num + 1, buffer);
-		line_num++;
-	}
-	prefresh(text_pad, 0, 0, 2, 2, y_max - 3, x_max - 2);
-
-	getchar();
+	} while (ch != 27);
 }
